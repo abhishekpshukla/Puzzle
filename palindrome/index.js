07781ae00ea402ed8b6fb668cc2424374223c5d1
Abhishek Shukla <shukla_ab@India.XoriantCorp.com>
2019-09-02 07:20:36 +0530

    first commit

---
+++ palindrome/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1]
+    });
+}
+
+module.exports = palindrome;
+
+
+// solution 1:
+// function palindrome(str) {
+//     return str.split('').reverse().join('') === str
+// }
+
+// Solution 2:
+// function palindrome(str) {
+//     let newChar = str.split('').reduce((rev, char) => {
+//         return (char + rev);
+//     });
+
+//     return (newChar === str);
+// }
---
+++ reversestring/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// Solution 3 best
+function reverse(str) {
+    return str.split('').reduce((rev, char) =>
+        char + rev
+        , '');
+}
+
+module.exports = reverse;
+
+
+// Solution 1
+// function reverse(str) {
+//     // return str.split('').reverse().join('');
+// }
+
+// Solution 2
+// function reverse(str) {
+//     let reverseStr = '';
+//     for(let char of str) {
+//         reverseStr = char + reverseStr;
+//     }
+//     return reverseStr;
+// }
+
+// Solution 3
